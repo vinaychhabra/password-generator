4e9b162ee67318c496f6d7504333d08b720ed0fe
vinaychhabra <vchhabra10feb@gmail.com>
2024-03-23 00:17:31 -0400

    some styling added and basic ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 function App() {
   return (
     <>
-      <p>This React Vite with Tailwind Boilerplate</p>
+      <h1 className="text-orange-500 mx-auto text-center text-3xl my-4 shadow-md">Password Generator</h1>
+      <div className="w-full fixed bottom-4 text-center mx-auto text-white"><p className="text-center">Designed and Developed by Vinay Chhabra</p></div>  
     </>
   )
 }
